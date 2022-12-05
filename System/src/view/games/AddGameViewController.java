146198ed07f449fdf681083bab5dfcf663fb5312
grumblewoes <culturuse@gmail.com>
2022-12-05 09:08:18 +0100

    added check condition to submission

--- System/src/view/games/AddGameViewController.java
+++ System/src/view/games/AddGameViewController.java
@@ -46,10 +46,12 @@ public class AddGameViewController extends ViewController
     reset();
   }
 
+  //submits game to game list. first checks that all fields are set. try-catch catches number formatting exceptions, fx.
+  //if a name gets submitted in the owner field instead of an id.
   @FXML
   void submitGame() {
-
-    try {
+    try
+    {
       String title = titleBox.getText();
       String players = playersBox.getText();
       int owner = Integer.parseInt(ownerBox.getText());
@@ -57,15 +59,20 @@ public class AddGameViewController extends ViewController
       String description = descriptionBox.getText();
       String genre = genreBox.getText();
 
-      Game game = new Game(title, clubAssociate, genre, players, description);
-      model.addGame(game);
-      viewHandler.openView("gameList");
+      if (title.equals("") || players.equals("") || owner == 0 || description.equals("") || genre.equals(""))
+        errorLabel.setText("Make sure all fields are filled before submission.");
+
+      else
+      {
+        Game game = new Game(title, clubAssociate, genre, players, description);
+        model.addGame(game);
+        viewHandler.openView("gameList");
+      }
     }
     catch (NumberFormatException e)
     {
-      errorLabel.setText("That owner's ID does not exist in the system. Check that you entered the ID correctly.");
+      errorLabel.setText("Make sure to enter the owner using their ID.");
     }
-
   }
 
   @Override public void reset()
